6679f2be73bc0ca40380f27ed2f094a9a17f1ea2
Rittika Shaw <shaw.rittika15@gmail.com>
2025-04-01 17:07:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Tictactoe from './components/tictactoe/tictactoe'; // Use capitalized 'Tictactoe'
+
+function App() {
+  return (
+    <div>
+      <Tictactoe /> {/* Use capitalized 'Tictactoe' here */}
+    </div>
+  );
+}
+
+export default App;
